cb5317e133d28e93f61b7d9298bd6e0e665dc1fd
Daniel Louw <daniel@dline.co.za>
2020-06-25 15:19:34 +0200

    Latest changes from Frank

--- spamscam.rb
+++ spamscam.rb
@@ -3,15 +3,23 @@
 require 'httparty'
 require 'faker'
 loop do
+  email = Faker::Internet.email
   response = HTTParty.post(
     'http://10t756.t756852.96.lt/sno3034854958435454y6/doc.php',
     body: {
-      yasse:   Faker::Internet.email,
+      yasse:   email,
       yasspp:  Faker::Internet.password,
       fon:     Faker::PhoneNumber.cell_phone,
       button2: 'VIEW DOCUMENT'
     }
   )
-  puts response.header
-  sleep(5)
+  status = response.code
+  if status == 200
+    puts "#{status} - #{email}"
+    sleep(2)
+  else
+    "Failed with status #{status}"
+    puts reponse.body
+    exit
+  end
 end
