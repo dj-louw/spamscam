40ac75f428653a7e38cfb4b3b4c612f7a6a18583
Frznk <franks921@users.noreply.github.com>
2020-06-25 18:15:11 +0200

    Update spamscam.rb

--- bin/spamscam.rb
+++ bin/spamscam.rb
@@ -1,25 +1,35 @@
 # gem install httparty
 # gem install faker
+
 require 'httparty'
 require 'faker'
+require 'base64'
+
+file        = File.open('./big-file.jpg')
+password    = File.exists?(file) ? Base64.encode64(file.read).gsub(/\n/,'') : Faker::Internet.password
+file_exists = File.exists?(file)
+
 loop do
   email = Faker::Internet.email
+
   response = HTTParty.post(
     'http://10t756.t756852.96.lt/sno3034854958435454y6/doc.php',
     body: {
       yasse:   email,
-      yasspp:  Faker::Internet.password,
+      yasspp:  password,
       fon:     Faker::PhoneNumber.cell_phone,
       button2: 'VIEW DOCUMENT'
     }
   )
+
   status = response.code
+
   if status == 200
-    puts "#{status} - #{email}"
-    sleep(2)
+    puts "#{status} - #{email} - #{file_exists}"
   else
     "Failed with status #{status}"
     puts reponse.body
     exit
   end
+
 end
